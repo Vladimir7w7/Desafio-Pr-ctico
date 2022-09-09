babca3513e38578abf8c46e80dfd4216650a9c30
Byron Amaya <60157235+bamaya@users.noreply.github.com>
2022-09-09 09:41:19 -0600

    Update1.1

--- ejercicio1/src/App.js
+++ ejercicio1/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Ejercicio1
-        </a>
-      </header>
-    </div>
-  );
+import Form from './components/Form';
+const App = () => {
+return (
+<div className="App">
+<div className="App-content">
+<p>
+Datos del Paciente
+</p>
+<Form/>
+</div>
+</div>
+);
 }
-
 export default App;
---
+++ ejercicio1/src/components/Form.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import Todo from '../components/Todo';
+
+const Form = () => {
+    const [number, setNumber] = useState({})
+    const [todo, setTodo] = useState({})
+    const [todos,setTodos] = useState ([])
+const handleChange1 = e => setNumber
+({[e.target.name]: e.target.value})
+const handleChange = e => setTodo
+({[e.target.name]: e.target.value})
+const handleClick = e => {
+    if(Object.keys(todo).length === 0 || todo.todo.trim() ==='' || Object.keys(number).length === 0 || number.number.trim() ==='') 
+    {alert ('el campo no puede estar vacio')
+    return
+}
+    setTodos([...todos, number, todo])
+}
+
+const deleteTodo = indice => {
+    const newTodos = [...todos]
+    newTodos.splice(indice,1)
+    setTodos(newTodos)
+}
+return (
+    <>
+    <form onSubmit = { e => e.preventDefault()}>
+        <label>Nombres:</label><br/>
+        <input type="text" name="number" 
+        onChange= {handleChange1}/><br/>
+        <label>Apellidos:</label><br/>
+        <input type="text" name="todo" 
+        onChange= {handleChange}/>
+        <button onClick={handleClick}>agregar</button>
+    </form>
+    {
+        todos.map((value, index) =>
+         (<Todo todo={value.todo} key={index} index={index}
+            deleteTodo={deleteTodo} number={value.number}
+         />))
+    }
+</>
+)
+}
+export default Form
