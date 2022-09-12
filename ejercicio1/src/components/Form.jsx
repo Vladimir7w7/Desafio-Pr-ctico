f07ec6d86c4da4aceac8dc7ea3ab905c61d6fa3e
Byron Amaya <60157235+bamaya@users.noreply.github.com>
2022-09-11 19:53:32 -0600

    Home v1

--- ejercicio1/src/components/Form.jsx
+++ ejercicio1/src/components/Form.jsx
@@ -11,7 +11,6 @@ const Form = () => {
     const [direccion, setDireccion] = useState({})
     const [municipio, setMunicipio] = useState({})
     const [departamento, setDepartamento] = useState({})
-    const [todo, setTodo] = useState({})
     const [todos,setTodos] = useState ([])
 
 const handleChange0 = e => setNombres
@@ -33,14 +32,14 @@ const handleChange7 = e => setMunicipio
 const handleChange8 = e => setDepartamento
 ({[e.target.name]: e.target.value})
 const handleClick = e => {
-    if(Object.keys(nombres).length === 0 || nombres.nombres.trim() ===''||Object.keys(apellidos).length === 0 || apellidos.apellidos.trim() ===''
-    ||Object.keys(sexo).length === 0 || sexo.sexo.trim() ===''||Object.keys(dui).length === 0 || dui.dui.trim() ===''||Object.keys(direccion).length === 0 || direccion.direccion.trim() ===''
-    ||Object.keys(municipio).length === 0 || municipio.municipio.trim() ===''||Object.keys(departamento).length === 0 || departamento.departamento.trim() ===''
-    ||Object.keys(fecha).length === 0 || fecha.fecha.trim() ==='')
-    {alert ('el campo no puede estar vacio')
-    return
-}
-    setTodos([...todos, todo,nombres,apellidos,sexo,dui,fecha,nacionalidad,direccion,municipio,departamento])
+     if(Object.keys(nombres).length === 0 || nombres.nombres.trim() ===''||Object.keys(apellidos).length === 0 || apellidos.apellidos.trim() ===''
+     ||Object.keys(sexo).length === 0 || sexo.sexo.trim() ===''||Object.keys(dui).length === 0 || dui.dui.trim() ===''||Object.keys(direccion).length === 0 || direccion.direccion.trim() ===''
+     ||Object.keys(municipio).length === 0 || municipio.municipio.trim() ===''||Object.keys(departamento).length === 0 || departamento.departamento.trim() ===''
+     ||Object.keys(fecha).length === 0 || fecha.fecha.trim() ==='')
+     {alert ('el campo no puede estar vacio')
+     return
+ }
+    setTodos([todos,nombres,apellidos,sexo,dui,fecha,nacionalidad,direccion,municipio,departamento])
 }
 //Validacion de letras
 
@@ -86,7 +85,7 @@ return (
     {
         todos.map((value, index) =>
          (<Todo todo={value.todo} key={index} index={index}
-          deleteTodo={deleteTodo} nombres={value.nombres} apellidos={value.apellidos} sexo={value.sexo} dui={value.dui} fecha={value.fecha} nacionalidad={value.nacionalidad}
+          deleteTodo={deleteTodo} nombres= {value.nombres} apellidos={value.apellidos} sexo={value.sexo} dui={value.dui} fecha={value.fecha} nacionalidad={value.nacionalidad}
           direccion={value.direccion} municipio={value.municipio} departamento={value.departamento}
          />))
     }
--- ejercicio1/src/components/Todo.jsx
+++ ejercicio1/src/components/Todo.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-const Todo = ({todo,index,deleteTodo,nombres,apellidos,sexo,dui,fecha,nacionalidad,direccion,municipio,departamento}) => {
+const Todo = ({nombres,apellidos,sexo,dui,fecha,nacionalidad,direccion,municipio,departamento}) => {
 return (
 <>
     <div className="list">
-        <h3> {nombres}</h3>
-        <h3> {apellidos}</h3>
-        <h3> {sexo}</h3>
-        <h3> {dui}</h3>
-        <h3> {fecha}</h3>
-        <h3> {nacionalidad}</h3>
-        <h3> {direccion}</h3>
-        <h3> {municipio}</h3>
-        <h3> {departamento}</h3>
+        <h3>{nombres}</h3>
+        <h3>{apellidos}</h3>
+        <h3>{sexo}</h3>
+        <h3>{dui}</h3>
+        <h3>{fecha}</h3>
+        <h3>{nacionalidad}</h3>
+        <h3>{direccion}</h3>
+        <h3>{municipio}</h3>
+        <h3>{departamento}</h3>
         </div>
      
     
